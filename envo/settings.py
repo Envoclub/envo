9a449241078c143bfa22fd985f6b5521a12401ca
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-05-31 00:05:41 +0100

    Intitializing Secrets commit

--- envo/settings.py
+++ envo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from secrets import django_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5^jly7#y+_3lweus@7ma24*ptm-w-^=a6khi1=%#ehwr9*(-%z'
+SECRET_KEY = django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
